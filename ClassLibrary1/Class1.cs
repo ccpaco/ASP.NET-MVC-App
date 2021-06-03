303ffb6a178ca4848e686c53603b45d2984a0020
christian <christian@DESKTOP-E3SMTHJ>
2021-06-03 13:39:27 -0800

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// EF for DB 
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+    }
+}
+
+//part of cartController, 6/3/21 lecture on Authentication
+/*string userId = Request.Form["UserId"];
+string password = Request.QueryString["Password"];
+string guid = Guid.NewGuid().ToString();*/
